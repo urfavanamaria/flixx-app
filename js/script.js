50497a8790c5f6b9485a9dd67e54220333c6ce57
Anamaria <natia.anamaria@gmail.com>
2025-06-24 23:28:37 +0400

    Pagination for search added

--- js/script.js
+++ js/script.js
@@ -5,6 +5,7 @@ const global = {
     type: '',
     page: 1,
     totalPages: 1,
+    totalResults: 0,
   },
   api: {
     apiKey: '6b3b7fd245142e73db0300908fc3b087',
@@ -210,7 +211,7 @@ async function displayShowDetails() {
             <li><span class="text-secondary">Number of Seasons:</span> ${
               show.number_of_seasons
             }</li>
-            <li><span class="text-secondary">last Episode To Air:</span> ${
+            <li><span class="text-secondary">Last Episode To Air:</span> ${
               show.last_episode_to_air.name
             }</li>
             <li><span class="text-secondary">Status:</span> ${show.status}</li>
@@ -258,7 +259,11 @@ async function search() {
   global.search.term = urlParams.get('search-term');
 
   if (global.search.term !== '' && global.search.term !== null) {
-    const { results, total_pages, page } = await searchAPIData();
+    const { results, total_pages, page, total_results } = await searchAPIData();
+
+    global.search.page = page;
+    global.search.totalPages = total_pages;
+    global.search.totalResults = total_results;
 
     if (results.length === 0) {
       showAlert('No results found');
@@ -274,6 +279,11 @@ async function search() {
 }
 
 function displaySearchResults(results) {
+  // Clear previous results
+  document.querySelector('#search-results').innerHTML = '';
+  document.querySelector('#search-results-heading').innerHTML = '';
+  document.querySelector('#pagination').innerHTML = '';
+
   results.forEach((result) => {
     const div = document.createElement('div');
     div.classList.add('card');
@@ -311,7 +321,50 @@ function displaySearchResults(results) {
           </div>
           `;
 
+    document.querySelector('#search-results-heading').innerHTML = `
+              <h2>${results.length} of ${global.search.totalResults} Results for ${global.search.term}</h2>
+    `;
+
     document.querySelector('#search-results').appendChild(div);
+  });
+
+  displayPagination();
+}
+
+// Create & Display Pagination For Search
+function displayPagination() {
+  const div = document.createElement('div');
+  div.classList.add('pagination');
+  div.innerHTML = `
+    <button class="btn btn-primary" id="prev">Prev</button>
+    <button class="btn btn-primary" id="next">Next</button>
+    <div class="page-counter">Page ${global.search.page} of ${global.search.totalPages}</div>
+    `;
+
+  document.querySelector('#pagination').appendChild(div);
+
+  //Disable prev button if on first page
+  if (global.search.page === 1) {
+    document.querySelector('#prev').disabled = true;
+  }
+
+  // Disable next button if on last page
+  if (global.search.page === global.search.totalPages) {
+    document.querySelector('#next').disabled = true;
+  }
+
+  // Next page
+  document.querySelector('#next').addEventListener('click', async () => {
+    global.search.page++;
+    const { results, total_pages } = await searchAPIData();
+    displaySearchResults(results);
+  });
+
+  // Prev page
+  document.querySelector('#prev').addEventListener('click', async () => {
+    global.search.page--;
+    const { results, total_pages } = await searchAPIData();
+    displaySearchResults(results);
   });
 }
 
@@ -392,7 +445,7 @@ async function searchAPIData() {
   showSpinner();
 
   const response = await fetch(
-    `${API_URL}search/${global.search.type}?api_key=${API_KEY}&language=en-US&query=${global.search.term}`
+    `${API_URL}search/${global.search.type}?api_key=${API_KEY}&language=en-US&query=${global.search.term}&page=${global.search.page}`
   );
 
   const data = await response.json();
@@ -400,16 +453,6 @@ async function searchAPIData() {
   hideSpinner();
 
   return data;
-}
-
-// Show Alert
-function showAlert(message, className = 'error') {
-  const alertEl = document.createElement('div');
-  alertEl.classList.add('alert', className);
-  alertEl.appendChild(document.createTextNode(message));
-  document.querySelector('#alert').appendChild(alertEl);
-
-  setTimeout(() => alertEl.remove(), 3000);
 }
 
 function showSpinner() {
@@ -428,6 +471,16 @@ function highlightActiveLink() {
       link.classList.add('active');
     }
   });
+}
+
+// Show Alert
+function showAlert(message, className = 'error') {
+  const alertEl = document.createElement('div');
+  alertEl.classList.add('alert', className);
+  alertEl.appendChild(document.createTextNode(message));
+  document.querySelector('#alert').appendChild(alertEl);
+
+  setTimeout(() => alertEl.remove(), 3000);
 }
 
 function addCommasToNumber(number) {
